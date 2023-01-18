8630425eca03051bc8348522153aacb8ddfbc5cb
Chris Sandvik <chris.sandvik@gmail.com>
2023-01-17 22:10:07 -0500

    Clean up all rollup conent

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,19 @@ const isCjs = BABEL_ENV !== undefined && BABEL_ENV === "cjs";
 module.exports = {
   plugins: ["@babel/plugin-transform-runtime"],
   presets: [
-    "@babel/preset-typescript",
     [
       "@babel/preset-env",
       {
+        targets: {
+          browsers: "> 0.25%, not dead",
+          node: "12",
+        },
         bugfixes: true,
         modules: isCjs ? "commonjs" : false,
         loose: true,
       },
+      "minify",
     ],
+    "@babel/preset-typescript",
   ],
 };
--- jest.config.js
+++ jest.config.js
@@ -61,14 +61,7 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "json",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "node"
-  // ],
+  moduleFileExtensions: ["js", "ts"],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
